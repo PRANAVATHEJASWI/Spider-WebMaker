4e52c34f2ef4e661109972c234d331a7489b8028
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-10-30 08:45:46 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -151,9 +151,3 @@ async def extract_resume(input_data: FilePath):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Processing failed: {e}")
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
-
