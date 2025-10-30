479877cdbb4963fe9b775d0c234c286e304fd647
Pranava Thejaswi N M <96561922+PRANAVATHEJASWI@users.noreply.github.com>
2025-10-30 08:23:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -153,4 +153,7 @@ async def extract_resume(input_data: FilePath):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+
